760b4ced2bc212dc0acdaf901c6cd343a09df43b
Daniel Earwicker <dan@earwicker.com>
2015-02-19 20:02:35 +0000

    Separate knockout-execute and knockout-promiseComputed libs as they are separate layers, some corrections suggested by Michael Best, also a further check to validate the first arg to ko.execute

--- index.js
+++ index.js
@@ -19,25 +19,33 @@ function setup() {
         '        Response is: <span data-bind="text: response"></span>' +
         '    <!-- /ko -->' +
         '    <!-- ko execute: mySideEffects --><!-- /ko -->' +
+        '    <div data-bind="text: pcm"></div>' +
         '</div>';
 
     container.innerHTML = html;
 
     var message = ko.observable(location.hash === "#old" ? "nonpure" : "pure"),
-        activated = ko.observable(true),
+        activated = ko.observable(false),
         response = ko.promiseComputed("", function() {
             return HTTP.get("bracketize/" + message() + counter())
                        .catch(function() { return ""; });
         });
+
+    var pcm = ko.pureComputed(message);
+
+    ko.execute(pcm, function() {
+        console.log("Here's a side-effect: " + message());
+    });
 
     ko.applyBindings({
         message: message,
         activated: activated,
         response: response,
         mySideEffects: ko.pureComputed(function() {
-            console.log("Here's a side-effect: " +
-                message() + " and " + activated());
-        })
+            /*console.log("Here's a side-effect: " +
+                message() + " and " + activated());*/
+        }),
+        pcm: pcm
     }, container);
 }
 
